f6716c16c4859a02221256522a9178d3d158a058
Jhin <engkim0826@hotmail.com>
2021-02-28 16:58:13 +0900

    LaunchAPI修正

--- start/server/src/datasources/launch.js
+++ start/server/src/datasources/launch.js
@@ -6,7 +6,7 @@ class LaunchAPI extends RESTDataSource {
         this.baseURL = 'https://api.spacexdata.com/v2/';
     }
     async getAllLaunches() {
-        const response = this.get('launches');
+        const response = await this.get('launches');
         return Array.isArray(response) ? response.map(launch => this.launchReducer(launch)) : [];
     }
 
